4e01b67cabb7fb60f6a0eccd7d61b76c49a0c13c
Garret Kanwischer <kanwischer.garret@gmail.com>
2023-04-03 16:56:46 -0500

    Completed stretch goals

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -68,6 +68,12 @@ for (changedSupplies of supplyChanges){
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
 
+let sum = 0;
+for (let i = 0; i < supplyChanges.length; i++){
+    sum += supplyChanges[i];
+}
+console.log(sum);
+
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
 //    There are 572 parts in total, and each box holds 7 parts.
@@ -75,3 +81,15 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+
+let remainingParts = 572;
+let filledBoxes = 0;
+
+while (remainingParts >= 7){
+    remainingParts -= 7;
+    filledBoxes++;
+}
+
+console.log('The total number of boxes filled was', filledBoxes, 
+'and the number of remaining parts was', remainingParts + ".");
+
